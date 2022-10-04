e0e9bb1e0a19c25047a6ec5da94a667b49b8f103
Chandrakant Naik <chandrakant.rvce@gmail.com>
2022-10-04 19:23:13 +0000

    Updated make_predict_azure_app.sh with url of newly provisioned azure web app

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-services.azurewebsites.net:$PORT/predict
+     -X POST https://flask-ml-app-services.azurewebsites.net:$PORT/predict
      # TODO: Replace the <yourappname> with your application name 
